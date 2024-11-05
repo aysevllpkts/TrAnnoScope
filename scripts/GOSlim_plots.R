83d8b4291721316540d99c225c28c67ed7a411f5
aysevllpkts <aysevilpektas@gmail.com>
2024-11-05 23:46:11 +0100

    Update GOSlim_plots.R - Top20 GoTerms

--- scripts/GOSlim_plots.R
+++ scripts/GOSlim_plots.R
@@ -23,9 +23,8 @@ save_plot <- function(plot, filename) {
   log_message(paste("Saved plot:", filename))
 }
 
-
 # Log the start of the script
-log_message("#---------| Starting GOSlim analysıs |----------#")
+log_message("#---------| Starting GOSlim analysis |----------#")
 
 # Log the received arguments
 log_message(paste("File:", file))
@@ -37,74 +36,58 @@ log_message(paste("Output directory:", outdir))
 log_message("Reading GOSlims data file...")
 dataGOSlims <- read.delim(file, header = FALSE, sep = "\t")
 
-# tidy dataset
+# Tidy dataset
 log_message("Tidying dataset")
-dataGOSlims <- dataGOSlims %>% 
+dataGOSlims <- dataGOSlims %>%
   select(V1, V3, V4) %>%
+  filter(!(V3 == V1)) %>% # Remove sub-terms with the same name as main group
   mutate(V1 = factor(V1, levels = c("cellular_component", "molecular_function", "biological_process"),
-                             labels = c("Cellular Component", "Molecular Function", "Biological Process"))) %>%
-  mutate(V3 = str_to_title(V3))
+                    labels = c("Cellular Component", "Molecular Function", "Biological Process"))) %>%
+  mutate(V3 = str_to_title(V3)) %>%
+  group_by(V1) %>%
+  slice_max(order_by = V4, n = 20) # Select top 20 terms for each main group
+
 names(dataGOSlims) <- c("category", "goslim", "count")
 head(dataGOSlims)
 
-
 # Create plots
 log_message("Creating and saving plots")
-
 custom_colors <- c("green", "blue", "red")
 
-pALL <- dataGOSlims %>% filter(count != 0) %>% 
-  ggplot(aes(x = reorder(goslim,count), y = count, fill = category)) +
-  facet_wrap(~category, scales = "free_y", ncol = 3) +
-  scale_fill_manual(values = custom_colors[1:length(unique(dataGOSlims$category))]) + 
+pALL <- dataGOSlims %>%
+  ggplot(aes(x = reorder(goslim, count), y = count, fill = category)) +
+  facet_wrap(~category, scales = "free", ncol = 3) +
+  scale_fill_manual(values = custom_colors[1:length(unique(dataGOSlims$category))]) +
   coord_flip() +
-  geom_bar(stat = "identity", width=.5) +  # Create bar plots +
-  labs(x="GOSlim Classification",y="Number of Sequences")+
-  theme(axis.text=element_text(size=10))+theme(text = element_text(size = 15))+
+  geom_bar(stat = "identity", width=.5) +
+  labs(x="GOSlim Classification", y="Number of Sequences") +
+  theme(axis.text=element_text(size=10), text = element_text(size = 15)) +
   theme_bw() +
-  theme(axis.text.x=element_text(size=10,angle=0))+theme(axis.title=element_text(size=15,face="bold"))+
-  ggtitle(paste(sample_name,"GOSlims Distribution",sep=" "))+
-  theme(plot.title = element_text(family="sans", colour = "black", size = rel(1.1)*1, face = "bold"), legend.position = "none")
+  theme(axis.text.x=element_text(size=10, angle=0),
+        axis.title=element_text(size=15, face="bold")) +
+  ggtitle(paste(sample_name, "GOSlims Distribution", sep=" ")) +
+  theme(plot.title = element_text(family="sans", colour = "black", size = rel(1.1)*1, face = "bold"),
+        legend.position = "none")
 
 # Save
 save_plot(pALL, paste(outdir, sample_name, "_ALL_GOSlims.", fmt, sep = ""))
 
-
-pCC <- dataGOSlims %>% filter(count != 0 & category == 'Cellular Component') %>%
-    ggplot(aes(x=reorder(goslim,count), y=count))+
-    geom_bar(stat="identity", fill="green", width=.5)+
-    coord_flip()+labs(x="Classification",y="Number of Sequences")+
+# Plot for each category
+plot_category <- function(category_name, color, filename_suffix) {
+  plot <- dataGOSlims %>% filter(category == category_name) %>%
+    ggplot(aes(x = reorder(goslim, count), y = count)) +
+    geom_bar(stat = "identity", fill = color, width = .5) +
+    coord_flip() + labs(x = "Classification", y = "Number of Sequences") +
     theme_bw() +
-    ggtitle(paste(sample_name,"Cellular Component GOslims",sep=" "))+
+    ggtitle(paste(sample_name, category_name, "GOslims", sep=" ")) +
     theme(plot.title = element_text(family="sans", colour = "black", size = rel(1.1)*1, face = "bold"))
+  
+  save_plot(plot, paste(outdir, sample_name, filename_suffix, fmt, sep = ""))
+}
 
-# Save
-save_plot(pCC, paste(outdir, sample_name, "_Cellular_Component_GOSlims.", fmt, sep = ""))
-
-
-
-pMF <- dataGOSlims %>% filter(count != 0 & category == 'Molecular Function')  %>%
-    ggplot(aes(x=reorder(goslim,count), y=count))+
-    geom_bar(stat="identity", fill="blue", width=.5)+
-    theme_bw() +
-    coord_flip()+labs(x="Classification",y="Number of Sequences")+
-    ggtitle(paste(sample_name,"Molecular Function GOSlims",sep=" "))+
-    theme(plot.title = element_text(family="sans", colour = "black", size = rel(1.1)*1, face = "bold"))
-
-# Save
-save_plot(pMF, paste(outdir, sample_name, "_Molecular_Function_GOSlims.", fmt, sep = ""))
-
-
-pBP <- dataGOSlims %>% filter(count != 0 & category == 'Biological Process')  %>%
-    ggplot(aes(x=reorder(goslim,count), y=count))+
-    geom_bar(stat="identity", fill="red", width=.5)+
-    theme_bw() +
-    coord_flip()+labs(x="Classification",y="Number of Sequences")+
-    ggtitle(paste(sample_name,"Biological Process GOSlims",sep=" "))+
-    theme(plot.title = element_text(family="sans", colour = "black", size = rel(1.1)*1, face = "bold"))
-
-# Save
-save_plot(pBP, paste(outdir, sample_name, "_Biological_Process_GOSlims.", fmt, sep = ""))
+plot_category("Cellular Component", "green", "_Cellular_Component_GOSlims.")
+plot_category("Molecular Function", "blue", "_Molecular_Function_GOSlims.")
+plot_category("Biological Process", "red", "_Biological_Process_GOSlims.")
 
 # Log the end of the script
 log_message("#-----------| GOSlims analysis finished |-----------#")
